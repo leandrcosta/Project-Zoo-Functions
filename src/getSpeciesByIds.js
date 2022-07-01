dbd962af43d56e36cc8b58f0cfb51f3cc9028e4d
Leandro Costa <leandrocostamdc@gmail.com>
2022-07-01 17:06:13 -0300

    Requisitos 1 e 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  const getAnimals = [];
+  ids.forEach((specie) => getAnimals.push(species.find(({ id }) => id === specie)));
+  return getAnimals;
 }
 
 module.exports = getSpeciesByIds;
